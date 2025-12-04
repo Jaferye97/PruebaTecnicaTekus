5a1344ab1bde7fd012e37144e56448e96f66b005
Yeimer Jaramillo <andresjara0897@hotmail.com>
2025-12-03 22:07:44 -0500

    feat (backend): add base Entity Framework configuration for read and write mappings

---
+++ backend/RepositoryEntityFrameworkSqlServer/Context/EntityDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RepositoryEntityFrameworkSqlServer.Context
+{
+    public class EntityDbContext : DbContext
+    {
+        public EntityDbContext()
+        {
+        }
+
+        public EntityDbContext(DbContextOptions<EntityDbContext> options)
+        : base(options)
+        { }
+    }
+}
