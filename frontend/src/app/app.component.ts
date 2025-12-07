391350d0e32168c219c4ea638b1508cb7bf27726
Yeimer Jaramillo <andresjara0897@hotmail.com>
2025-12-07 01:14:26 -0500

    feat (frontend): add Sidebar component with navigation functionality

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
+
+import { SidebarComponent } from './components-general/layout/sidebar/sidebar.component';
+
+const myComponents = [SidebarComponent];
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatSidenavModule, ...myComponents],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'FrontendTekus';
---
+++ frontend/src/app/components-general/layout/sidebar/sidebar.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-sidebar',
+  standalone: true,
+  imports: [MatListModule, MatIconModule, RouterModule, CommonModule],
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css'],
+})
+export class SidebarComponent {
+  activeSection: string | null = null;
+
+  menu = [
+    {
+      title: 'Supplier',
+      key: 'supplier',
+      icon: 'storage',
+      items: [
+        {
+          title: 'All Suppliers',
+          key: 'all-suppliers',
+          route: '',
+          icon: 'storage',
+        },
+      ],
+    },
+  ];
+
+  toggleSection(key: string) {
+    this.activeSection = this.activeSection === key ? null : key;
+  }
+}
