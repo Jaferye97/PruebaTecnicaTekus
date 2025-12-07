a722c9441b35c488608d3132283363371c00898b
Yeimer Jaramillo <andresjara0897@hotmail.com>
2025-12-06 23:59:47 -0500

    feat (backend): add functionality to get countries by name filter

---
+++ backend/Application/UseCases/Country/IGetCountryByNameUseCase.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Models.Country;
+
+namespace Application.UseCases.Country
+{
+    public interface IGetCountryByNameUseCase
+    {
+        Task<List<CountryModel>> GetCountriesAsync(string name);
+    }
+}
---
+++ backend/CountriesApiClient/Model/NativeNameDetail.cs
@@ -0,0 +1,8 @@
+﻿namespace CountriesApiClient.Model
+{
+    internal class NativeNameDetail
+    {
+        public string Official { get; set; }
+        public string Common { get; set; }
+    }
+}
