99b8430744c1a05bfb942ebf64d10637630ff3a5
Yeimer Jaramillo <andresjara0897@hotmail.com>
2026-01-14 19:14:30 -0500

    refactor (backend): (RepositoryEntityFrameworkSqlServerV2) add base repository

---
+++ backend/RepositoryEntityFrameworkSqlServerV2/Repositories/IBaseRepository.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq.Expressions;
+
+namespace RepositoryEntityFrameworkSqlServerV2.Repositories;
+
+public interface IBaseRepository<TEntity, TModel, in TPrimary>
+        where TEntity : class
+        where TModel : class
+{
+    Task<TModel> GetAsync(TPrimary id);
+
+    Task<List<TModel>> GetAllAsync();
+
+    Task<List<TModel>> GetAllByIdAsync(IEnumerable<TPrimary> ids);
+
+    Task<TEntity> AddAsync(TModel model);
+
+    Task<IEnumerable<TEntity>> AddAsync(IEnumerable<TModel> models);
+
+    Task<TEntity> UpdateAsync(TModel model);
+
+    Task UpdateAsync(IEnumerable<TModel> models);
+
+    Task DeleteAsync(TModel model);
+
+    Task DeleteAsync(IEnumerable<TModel> models);
+
+    Task<List<TModel>> GetWithPredicateAsync(Expression<Func<TEntity, bool>> predicate);
+
+    Task<List<TModel>> GetAsync(
+        Expression<Func<TEntity, bool>> filter = null,
+        Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>>? orderBy = null,
+        params Expression<Func<TEntity, object>>[] includes);
+
+    Task<TModel> GetUniqueAsync(
+        Expression<Func<TEntity, bool>> filter = null,
+        Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>>? orderBy = null,
+        params Expression<Func<TEntity, object>>[] includes);
+
+    Task<int> CountAsync(Expression<Func<TEntity, bool>> predicate);
+}
