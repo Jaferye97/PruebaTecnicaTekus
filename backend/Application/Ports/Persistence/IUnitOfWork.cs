7a33884f5d8463680d1d01e699ac89407e697467
Yeimer Jaramillo <andresjara0897@hotmail.com>
2026-01-15 13:00:52 -0500

    refactor (backend): (RepositoryEntityFrameworkSqlServerV2) implement Unit of Work with EF Core repositories

---
+++ backend/Application/Ports/Persistence/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.Ports.Persistence;
+
+public interface IUnitOfWork : IDisposable
+{
+    Task BeginTransactionAsync();
+    Task CommitAsync();
+    Task RollbackAsync();
+    Task SaveChangesAsync();
+}
+
